f20c7e807cc20a2c4c31898ed9e19eeb52c7a4b0
amazing-compass <53112187+amazing-compass@users.noreply.github.com>
2024-10-23 13:06:27 -0700

    fix inventory type mismatch bug

--- Assets/Inventory/InventoryScript/OperatorInventorySO.cs
+++ Assets/Inventory/InventoryScript/OperatorInventorySO.cs
@@ -2,13 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-//运算符背包
 [CreateAssetMenu(fileName = "New Operator Inventory", menuName = "Inventory/Operator Inventory")]
 public class OperatorInventorySO : InventorySO
 {
-    // 
     protected override bool ItemsAreEqual(Item item1, Item item2)
     {
         if (item1 is OperatorSO op1 && item2 is OperatorSO op2)
@@ -17,5 +13,34 @@ public class OperatorInventorySO : InventorySO
         }
         return base.ItemsAreEqual(item1, item2);
     }
-}
 
+    public override void AddItem(Item newItem)
+    {
+        if (!(newItem is OperatorSO operatorItem))
+        {
+            Debug.LogError($"Attempted to add non-operator item to operator inventory: {newItem.GetType()}");
+            return;
+        }
+
+        Debug.Log($"Adding operator: {(operatorItem as OperatorSO).operatorChar}");
+
+        // 查找是否已存在相同的运算符
+        OperatorSO existingOperator = items.Find(item => 
+            item is OperatorSO op && 
+            (op as OperatorSO).operatorChar == operatorItem.operatorChar) as OperatorSO;
+
+        if (existingOperator != null)
+        {
+            // 如果存在，只增加计数
+            existingOperator.count++;
+            Debug.Log($"Increased count for operator {existingOperator.operatorChar}. New count: {existingOperator.count}");
+        }
+        else
+        {
+            // 如果不存在，使用原始实例添加到列表中
+            operatorItem.count = 1;
+            items.Add(operatorItem);
+            Debug.Log($"Added new operator {operatorItem.operatorChar} to inventory");
+        }
+    }
+}
