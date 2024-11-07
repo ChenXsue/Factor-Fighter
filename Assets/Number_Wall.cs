c31fe0a4bd05a54dd27576766547acfe2c935c00
reiirene <hsinli@usc.edu>
2024-11-07 14:24:59 -0800

    Number_wall

---
+++ Assets/Number_Wall.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number_Wall : MonoBehaviour
+{
+    public int areaSize;
+    public int givenSide;
+    public GameObject mathProblemPanel;
+    public GameObject operatorsPanel;
+    public GameObject input;
+    public static bool isGamePaused = false;
+    private string mathProblem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mathProblem = "What is ?";
+        // mathProblem = givenSide.ToString() + " * ? = " + areaSize.ToString();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PauseGame();
+            Debug.Log("Player has reached the number wall!");
+        }
+
+        mathProblemPanel.SetActive(true);
+        operatorsPanel.SetActive(true);
+        input.SetActive(true);
+    }
+
+    public static void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isGamePaused = true;
+        Debug.Log("Game paused");
+    }
+
+    public static void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isGamePaused = false;
+        Debug.Log("Game resumed");
+    }
+}
