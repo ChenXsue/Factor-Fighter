5c840ab16b0480a5883a53da8c4fb861652a2cfe
amazing-compass <53112187+amazing-compass@users.noreply.github.com>
2024-11-14 20:11:25 -0800

    change circle to square

--- Assets/Scripts/NumberCarrier.cs
+++ Assets/Scripts/NumberCarrier.cs
@@ -93,6 +93,7 @@ public class NumberCarrier : MonoBehaviour
         if (circleRenderer != null)
         {
             circleRenderer.color = Color.yellow;
+            circleRenderer.sortingOrder = 1; // 圆圈在最上层
         }
     }
 
